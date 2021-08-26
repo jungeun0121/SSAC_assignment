0294912ddbc74adec1277563487ea2eff91be33d
jungeun0121 <dodamdodam121@gmail.com>
2021-08-27 03:11:49 +0900

    weel03_Change To Function Expression

--- week03_Number BaseBall Game/NumberBaseball.js
+++ week03_Number BaseBall Game/NumberBaseball.js
@@ -4,26 +4,19 @@ const answerLen = 3;
 
 // 맞춰야 하는 3자리 수의 배열 생성(정답)
 let answerNum = new Array(answerLen);
-function makeNumber(){
-
+var makeNumber = function (){
     let candidate = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];    
-
     for (let i =0; i<answerLen; i++){
-
         answerNum[i] = candidate[Math.floor(Math.random() * candidate.length)];
         candidate.splice(answerNum[i],1);
-        console.log(candidate); // candidate 요소 삭제 확인
-        
+        // console.log(candidate); // candidate 요소 삭제 확인        
     }; 
-
     return answerNum;
-
-};
+}();
 
 
 // 정답과 입력값 비교
-function compareNumber(num1, num2, num3){
-
+const compareNumber = function (num1, num2, num3){
     // 유저 입력값 배열로 변환
     const playerNum = [num1, num2, num3];
 
@@ -43,9 +36,8 @@ function compareNumber(num1, num2, num3){
     
     // 비교 결과 확인
     console.log("%sS %sB %sO", strike, ball, out);
-    return (s, b, o)
-
+    return (strike, ball, out)
 };
 
-console.log("answer = ", makeNumber())
+console.log("answer >> ", makeNumber)
 compareNumber(1,2,3)
